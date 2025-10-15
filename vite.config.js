993adcb464c9738c232ea0f07a7d3d8722806d9c
Ashish Ranjan <coolashishranjan44551@gmail.com>
2025-10-15 15:44:03 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/ashish-Portfolio/",
-  esbuild: {
-    jsx: "automatic",
-  },
-});
+  plugins: [react()],
+  base: '/ashish-portfolio/', // must match the repo name exactly (case-sensitive)
+})
